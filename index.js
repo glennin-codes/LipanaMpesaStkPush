d185a87f594843827a36d9307ae2cd4846e9ec1a
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2022-12-17 21:22:50 +0300

    mpesaapi

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const axios = require("axios");
+const { response } = require("express");
 require("dotenv").config();
 
 //middlewares
@@ -17,59 +18,83 @@ app.get("/test", (req, res) => {
 app.listen(Port, () => {
   console.log(`app is listening on port :${Port}`);
 });
-//middlware function to generate tokens
-const generateToken= async(req,res,next)=>
-//getting the  auth ..by encoding both sonsumer key and 
-{
-    await axios.get("",{
-        headers:{
-            authorization:`Basic ${auth}`
-        }
+//middlware function to generate token
+const generateToken = async (req, res, next) =>
+  //getting the  auth ..by encoding both consumer key and consumerSecret
+  {
+    const secret = process.env.MPESA_SECRET;
+    const consumerKey = process.env.MPESA_CONSUMER_KEY;
+//AUTH
+const auth  = new Buffer.from(`${consumerKey}:${secret }`).toString(
+    "base64"
+  );
+
+    await axios.get("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials", { 
+        
+      headers: {
+        authorization: `Basic ${auth}`,
+      },
+    }).then((response)=>{
+        token=response.data.access_token
+        next()
+    }).catch((err)=>{
+        console.log(err.message);
     })
-}
+  };
+  //processing api request
 app.post("/stk", async (req, res) => {
   const phone = req.body.phone.substring(1);
   const amount = req.body.amount;
   res.json({ phone, amount });
-//timestamp
-const date=new Date();
-const timeStamp=date.getFullYear()+
-("0" + (date.getMonth() +1)).slice(-2) +
-("0" + (date.getDate() +1)).slice(-2) +
-("0" + (date.getHours() +1)).slice(-2) +
-("0" + (date.getMinutes() +1)).slice(-2) +
-("0" + (date.getSeconds() +1)).slice(-2) ;
+  //timestamp
+  const date = new Date();
+  const timeStamp =
+    date.getFullYear() +
+    ("0" + (date.getMonth() + 1)).slice(-2) +
+    ("0" + (date.getDate() + 1)).slice(-2) +
+    ("0" + (date.getHours() + 1)).slice(-2) +
+    ("0" + (date.getMinutes() + 1)).slice(-2) +
+    ("0" + (date.getSeconds() + 1)).slice(-2);
 
-const shortCode=process.env.MPESA_PAYBILL
-const passKey=process.env.MPESA_PASSKEY
+  const shortCode = process.env.MPESA_PAYBILL;
+  const passKey = process.env.MPESA_PASSKEY;
 
-//(The base64 string is a combination of Shortcode+Passkey+Timestamp)
-const password=new Buffer.from(shortCode + passKey +timeStamp).toString('base64');
+  //(The base64 string is a combination of Shortcode+Passkey+Timestamp)
+  const password = new Buffer.from(shortCode + passKey + timeStamp).toString(
+    "base64"
+  );
 
-
-  await axios.post(
-    "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
-    {
-      BusinessShortCode:shortCode,
-      Password:
-        password,
-      Timestamp: timeStamp ,
-      TransactionType: "CustomerPayBillOnline",//"CustomerBuyGoodsOnline"
-      Amount:amount,
-      PartyA: `254${phone}`,
-      PartyB: shortCode,
-      PhoneNumber: "254708374149",
-      CallBackURL: "https://mydomain.com/pat",
-      AccountReference: "Test",
-      TransactionDesc: "Test",
-    },{
-        headers:`Bearer ${  token}`
-    }
-  ).then((data)=>{
-    console.log(data);
-    res.status(200).json(data)
-  }).catch((err)=>{
-    console.log(err.message);
-    res.status(400).json(err.message)
-  })
+  await axios
+    .post(
+      "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+      {
+        BusinessShortCode: shortCode,
+        Password: password,
+        Timestamp: timeStamp,
+        TransactionType: "CustomerPayBillOnline", //"CustomerBuyGoodsOnline"
+        Amount: amount,
+        PartyA: `254${phone}`,
+        PartyB: shortCode,
+        PhoneNumber: "254708374149",
+        CallBackURL: " https://2b28-154-122-138-177.eu.ngrok.io/callback",
+        AccountReference: `254${phone}`,
+        TransactionDesc: "fee payment recieved ",
+      },
+      {
+        headers: `Bearer ${token}`,
+      }
+    )
+    .then((data) => {
+      console.log(data.data);
+      res.status(200).json(data.data);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(400).json(err.message);
+    });
 });
+
+app.post('/callback',(req,res)=>{
+    const callBackData=req.body.value
+
+})
